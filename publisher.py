422093dd59cbce7c0d6da8f67217d5bc66177700
Abhinav Sadhu <sadhu.Abhinav@gmail.com>
2021-10-03 11:15:28 +0530

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -1,6 +1,8 @@
 import paho.mqtt.client as mqtt
 from time import sleep
 import sys
+from dotenv import dotenv_values
+config = dotenv_values(".env")
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
